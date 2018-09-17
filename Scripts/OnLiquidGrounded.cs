6d8eeadf4ed2bcf1e48b83a05cf0105f747b7e05
nesh <kulplex@gmail.com>
2018-09-17 23:56:18 +0530

    first commit

---
+++ Scripts/OnLiquidGrounded.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class OnLiquidGrounded : MonoBehaviour
+{
+
+    [SerializeField] private Animator FluidAnimator;
+
+
+    void Start()
+    {
+        if (FluidAnimator == null)
+        {
+            FluidAnimator = GetComponent<Animator>();
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            FluidAnimator.SetBool("IsGrounded", true);
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            FluidAnimator.SetBool("IsGrounded", false);
+        }
+    }
+}
